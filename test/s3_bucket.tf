4e95b7ff0cc013df57af46dbf8edbdf238fa2df0
lcastrose <44179932+lcastrose@users.noreply.github.com>
2021-09-14 17:12:33 -0500

    Update s3_bucket.tf

--- test/s3_bucket.tf
+++ test/s3_bucket.tf
@@ -1,7 +1,7 @@
 resource "aws_s3_bucket" "foo" {
     bucket = "tf-test-trial"
-    force_destroy = true
-    versioning { enabled = true }
+    force_destroy = false
+    versioning { enabled = false }
     acl = "public-read-write"
      
 }
